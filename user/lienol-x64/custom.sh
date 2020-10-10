28d3dafa814b4e8af2a484e1c971f0a37ecd8895
hyird <31695447+hyird@users.noreply.github.com>
2020-10-11 05:16:10 +0800

    Update custom.sh

--- user/lienol-x64/custom.sh
+++ user/lienol-x64/custom.sh
@@ -5,7 +5,7 @@ echo "Test custom.sh"
 source ../version
 
 rm -rf package/lean/default-settings/files/zzz-default-settings
-wget https://raw.githubusercontent.com/hyird/Action-Openwrt/main/user/lienol-x64/default-settings -O package/lean/default-settings/files/zzz-default-settings
+wget https://raw.githubusercontent.com/hyird/Action-Openwrt/main/user/lienol-x64/default-settings -O package/default-settings/files/zzz-default-settings
 
 sed -i '92d'                                                                   package/system/opkg/Makefile
 sed -i  "s/L20\(.[0-9].[0-9]\{1,2\}\)/L20.$version/g" package/lean/default-settings/files/zzz-default-settings
