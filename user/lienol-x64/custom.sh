11154115f841d573b8f70cf347a28f5362266421
hyird <hyird@outlook.com>
2020-10-11 05:07:10 +0800

    x

---
+++ user/lienol-x64/custom.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Test custom.sh"
+
+source ../version
+
+rm -rf package/feeds/routing/olsrd/patches/012-gpsd.patch
+rm -rf package/lean/default-settings/files/zzz-default-settings
+wget  -O package/lean/default-settings/files/zzz-default-settings
+
+sed -i '92d'                                                                   package/system/opkg/Makefile
+sed -i  "s/L20\(.[0-9].[0-9]\{1,2\}\)/L20.$version/g" package/lean/default-settings/files/zzz-default-settings
+sed -i 's/DEPENDS.*/& \+luci-i18n-samba-zh-cn/g'  package/lean/autosamba/Makefile
+
+
+
+
+
