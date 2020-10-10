ee5ffd8d6ca062ea1a6d24a25f82a0e08b78ad7b
hyird <31695447+hyird@users.noreply.github.com>
2020-10-11 05:17:04 +0800

    Update custom.sh

--- user/lienol-x64/custom.sh
+++ user/lienol-x64/custom.sh
@@ -8,7 +8,7 @@ rm -rf package/lean/default-settings/files/zzz-default-settings
 wget https://raw.githubusercontent.com/hyird/Action-Openwrt/main/user/lienol-x64/default-settings -O package/default-settings/files/zzz-default-settings
 
 sed -i '92d'                                                                   package/system/opkg/Makefile
-sed -i  "s/L20\(.[0-9].[0-9]\{1,2\}\)/L20.$version/g" package/lean/default-settings/files/zzz-default-settings
+sed -i  "s/L20\(.[0-9].[0-9]\{1,2\}\)/L20.$version/g" package/default-settings/files/zzz-default-settings
 sed -i 's/DEPENDS.*/& \+luci-i18n-samba-zh-cn/g'  package/lean/autosamba/Makefile
 
 
