84602f42c14c877eaa678071a451f71b02931d0c
hyird <hyird@outlook.com>
2020-10-26 22:17:50 +0800

    x

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -5,15 +5,16 @@ echo "Apply custom.sh"
 sed -i 's/DEPENDS.*/& \+luci-i18n-mwan3-zh-cn/g'  package/lean/luci-app-syncdial/Makefile
 sed -i 's/DEPENDS.*/& \+kmod-usb-core +kmod-usb-ohci +kmod-usb-uhci +kmod-usb-storage +kmod-usb-storage-extras +kmod-usb2 +kmod-scsi-core/g'  package/lean/automount/Makefile
 
-git clone https://github.com/CCnut/feed-netkeeper.git -b LUCI-LUA-UCITRACK                                 package/nk
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git                             package/luci-theme-infinityfreedom
 git clone https://github.com/yangsongli/luci-theme-atmaterial.git                                        package/luci-theme-atmaterial
 git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06                                     package/luci-theme-argon-jerrykuku
 git clone https://github.com/tty228/luci-app-serverchan.git                                              package/luci-app-serverchan
 git clone https://github.com/vernesong/OpenClash.git -b master                                           package/luci-app-openclash
 git clone https://github.com/tindy2013/openwrt-subconverter.git                                          package/openwrt-subconverter
-
-
+git clone https://github.com/garypang13/luci-app-eqos								                     package/luci-app-eqos
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git                                         package/luci-theme-opentomcat
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git                                         package/luci-theme-opentomato
+git clone https://github.com/jerrykuku/luci-app-argon-config.git                                         package/luci-app-argon-config
 
 
       
