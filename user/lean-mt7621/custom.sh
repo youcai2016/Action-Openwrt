5599c324530c83ee8a36f6881bec2e27a265c02a
hyird <hyird@outlook.com>
2020-11-29 08:31:26 +0800

    R20.12.4

--- user/lean-mt7621/custom.sh
+++ user/lean-mt7621/custom.sh
@@ -7,6 +7,7 @@ source ../version
 rm -rf package/feeds/routing/olsrd/patches/012-gpsd.patch
 rm -rf package/lean/default-settings/files/zzz-default-settings
 wget https://raw.githubusercontent.com/coolsnowwolf/lede/99222f49fd9f63eef575ac34f251280e6e97f3b3/package/lean/default-settings/files/zzz-default-settings -O package/lean/default-settings/files/zzz-default-settings
+sed -i 's/DEPENDS.*/& \+luci-i18n-samba-zh-cn/g'  package/lean/autosamba/Makefile
 
 sed -i '92d'                                                                   package/system/opkg/Makefile
 sed -i '/shadow/d'                     package/lean/default-settings/files/zzz-default-settings
@@ -16,7 +17,10 @@ sed -i "s/https:/R20.0.0/g"  package/lean/default-settings/files/zzz-default-set
 sed -i  's/http:/snapshots/g'  package/lean/default-settings/files/zzz-default-settings
 sed -i  " 23i sed -i 's/http:/https:/g' /etc/opkg/distfeeds.conf"  package/lean/default-settings/files/zzz-default-settings
 sed -i  "s/R20\(.[0-9].[0-9]\{1,2\}\)/R20.$version/g" package/lean/default-settings/files/zzz-default-settings
-sed -i 's/DEPENDS.*/& \+luci-i18n-samba-zh-cn/g'  package/lean/autosamba/Makefile
+
+sed  "44i echo ' ' >> /etc/opkg/customfeeds.conf " package/lean/default-settings/files/zzz-default-settings
+sed  "45i echo 'src/gz openwrt_all https://openwrt.download/ExtraPackages/all' >> /etc/opkg/customfeeds.conf " package/lean/default-settings/files/zzz-default-settings
+sed  "46i echo 'src/gz openwrt_ipk https://openwrt.download/ExtraPackages/mipsel_24kc' >> /etc/opkg/customfeeds.conf " package/lean/default-settings/files/zzz-default-settings
 
 rm -rf feeds/packages/net/smartdns
 rm -rf package/lean/luci-theme-argon
