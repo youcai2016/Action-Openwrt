1ab78c44015b202fc7463b33358509373ea58a79
hyird <31695447+hyird@users.noreply.github.com>
2020-12-03 19:29:37 +0800

    Update custom.sh

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -14,10 +14,10 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06            
 git clone https://github.com/tty228/luci-app-serverchan.git                                              package/luci-app-serverchan
 git clone https://github.com/vernesong/OpenClash.git -b master                                           package/luci-app-openclash
 git clone https://github.com/tindy2013/openwrt-subconverter.git                                          package/openwrt-subconverter
-git clone https://github.com/garypang13/luci-app-eqos								                     package/luci-app-eqos
+git clone https://github.com/garypang13/luci-app-eqos								                                     package/luci-app-eqos
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git                                         package/luci-theme-opentomcat
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git                                         package/luci-theme-opentomato
 git clone https://github.com/jerrykuku/luci-app-argon-config.git                                         package/luci-app-argon-config
 git clone https://github.com/jerrykuku/lua-maxminddb.git                                                 package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git                                                 package/luci-app-vssr
-git clone https://github.com/apollo-ng/luci-theme-darkmatter.git		                                     package/luci-theme-darkmatter
+git clone https://github.com/hyird/luci-theme-darkmatter.git		                                         package/luci-theme-darkmatter
