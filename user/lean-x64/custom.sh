539e04ff603e113f87a9b587280422c245431c3a
hyird <31695447+hyird@users.noreply.github.com>
2020-12-18 16:16:43 +0800

    Update custom.sh

--- user/lean-x64/custom.sh
+++ user/lean-x64/custom.sh
@@ -18,7 +18,7 @@ sed -i  's/http:/snapshots/g'  package/lean/default-settings/files/zzz-default-s
 sed -i  " 23i sed -i 's/http:/https:/g' /etc/opkg/distfeeds.conf"  package/lean/default-settings/files/zzz-default-settings
 sed -i  "s/R20\(.[0-9].[0-9]\{1,2\}\)/R20.$version/g" package/lean/default-settings/files/zzz-default-settings
 
-sed -i "44i echo 'src/gz openwrt_all https://openwrt.download/ExtraPackages/all' >> /etc/opkg/customfeeds.conf " package/lean/default-settings/files/zzz-default-settings
+sed -i "44i echo 'src/gz openwrt_all https://openwrt.download/ExtraPackages/all' > /etc/opkg/customfeeds.conf " package/lean/default-settings/files/zzz-default-settings
 sed -i "45i echo 'src/gz openwrt_ipk https://openwrt.download/ExtraPackages/x86_64' >> /etc/opkg/customfeeds.conf " package/lean/default-settings/files/zzz-default-settings
 
 rm -rf feeds/packages/net/smartdns
