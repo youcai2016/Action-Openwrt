658e43ac81b954d6aa6e6cbd0ac6ca97a015f715
hyird <hyird@outlook.com>
2020-12-28 12:31:43 +0800

    x

--- user/lean-x64/custom.sh
+++ user/lean-x64/custom.sh
@@ -13,10 +13,10 @@ sed -i '92d'                                                                   p
 sed -i '/shadow/d'                     package/lean/default-settings/files/zzz-default-settings
 sed -i '/nas/d'                     package/lean/default-settings/files/zzz-default-settings
 sed -i "s/openwrt.proxy.ustclug.org/openwrt.download/g"  package/lean/default-settings/files/zzz-default-settings
-sed -i "s/https:/R21.0.0/g"  package/lean/default-settings/files/zzz-default-settings
+sed -i "s/https:/R20.0.0/g"  package/lean/default-settings/files/zzz-default-settings
 sed -i  's/http:/snapshots/g'  package/lean/default-settings/files/zzz-default-settings
 sed -i  " 23i sed -i 's/http:/https:/g' /etc/opkg/distfeeds.conf"  package/lean/default-settings/files/zzz-default-settings
-sed -i  "s/R21\(.[0-9].[0-9]\{1,2\}\)/R21.$version/g" package/lean/default-settings/files/zzz-default-settings
+sed -i  "s/R20\(.[0-9].[0-9]\{1,2\}\)/R21.$version/g" package/lean/default-settings/files/zzz-default-settings
 
 sed -i "44i echo 'src/gz openwrt_all https://openwrt.download/ExtraPackages/all' > /etc/opkg/customfeeds.conf " package/lean/default-settings/files/zzz-default-settings
 sed -i "45i echo 'src/gz openwrt_ipk https://openwrt.download/ExtraPackages/x86_64' >> /etc/opkg/customfeeds.conf " package/lean/default-settings/files/zzz-default-settings
