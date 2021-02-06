5f9595c11bbd47ea22b77f7e02f074227a3fa206
hyird <31695447+hyird@users.noreply.github.com>
2021-02-06 18:46:22 +0800

    Update custom.sh

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -25,7 +25,7 @@ git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git                
 git clone https://github.com/jerrykuku/luci-app-argon-config.git                                         package/luci-app-argon-config
 git clone https://github.com/jerrykuku/lua-maxminddb.git                                                 package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git                                                 package/luci-app-vssr
-git clone https://github.com/hyird/luci-theme-darkmatter.git		                                     package/luci-theme-darkmatter
+git clone https://github.com/hyird/luci-theme-darkmatter.git		                                         package/luci-theme-darkmatter
 
 sed -i 's/DEPENDS.*/& \+AdGuardHome/g'  package/adg/Makefile
 sed -i 's/DEPENDS.*/& \+kmod-tun +libcap-bin/g'  package/luci-app-openclash/luci-app-openclash/Makefile
